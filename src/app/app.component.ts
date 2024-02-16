b49c50936b00125bfe0a77ba8681ee0a1cdb145b
chandini02 <chandini0703@gmail.com>
2024-02-16 12:46:37 +0530

    first-commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,16 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'components-movie';
+  img1="https://m.media-amazon.com/images/M/MV5BY2JhODU1NmQtNjllYS00ZmQwLWEwZjYtMTE5NjA1M2YyMzdjXkEyXkFqcGdeQXVyMTQxNzMzNDI@._V1_.jpg";
+  img2="https://m.media-amazon.com/images/I/51UtV22RQPL._AC_UF1000,1000_QL80_.jpg";
+  img3="https://i5.walmartimages.ca/images/Enlarge/965/136/965136.jpg?odnHeight=612&odnWidth=612&odnBg=FFFFFF";
+  img4="https://m.media-amazon.com/images/M/MV5BMTgwOTk1NjQ0NV5BMl5BanBnXkFtZTcwNTk4MDI3NQ@@._V1_.jpg";
+  img5="https://m.media-amazon.com/images/M/MV5BNjE2ODYxNzE1NF5BMl5BanBnXkFtZTcwMzkzNzQ0OA@@._V1_.jpg";
+  t1="spy kids";
+  t2="spy kids-2";
+  t3="spy kids-3";
+  t4="spy kids-4";
+  t5="Baby Geniuses";
+
 }
+
